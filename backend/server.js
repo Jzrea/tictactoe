d6b40eda1db0bb59bfb7c6f5e9bacf99dd5b77b5
Jzrea <111337870+Jzrea@users.noreply.github.com>
2024-02-17 11:15:04 +0800

    pre deployment preparations

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,15 @@ if (process.env.NODE_ENV != "production") {
 app.use("/api/session", require("./routes/sessions.routes.js"));
 // #endregion
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+} else {
+    app.use("*", (req, res) => { res.status(404).json({ error: "access" }); });
+}
+
 app.use(errorHandler);
 
 module.exports = app;
